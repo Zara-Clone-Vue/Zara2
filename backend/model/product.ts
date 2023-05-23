ccdc01a88c7d1af547324becfa184fdc4be0de5a
SarhaneGhribi <sarhane.ghribi@gmail.com>
2023-05-23 17:43:42 +0100

    server and databse running

--- backend/model/product.ts
+++ backend/model/product.ts
@@ -1,62 +1,80 @@
-import connection from "../database/db";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+@Entity()
+export class product{
+    @PrimaryGeneratedColumn()
+    id!:number;
 
-const getAllProducts=(callback)=>{
-    connection.query("SELECT * FROM product",callback)
+    @Column()
+    clothesName!:string;
+
+    @Column()
+    image!:string;
+
+    @Column()
+    price!:number;
+
+    @Column()
+    category!:string;
 }
+// import connection from "../database/db";
 
-const getAdultCategory=(callback)=>{
-    connection.query("SELECT * FROM product WHERE category='adult'",callback)
-}
+// const getAllProducts=(callback)=>{
+//     connection.query("SELECT * FROM product",callback)
+// }
 
-const getKidsCategory=(callback)=>{
-    connection.query("SELECT * FROM product WHERE category='kids'",callback)
-}
+// const getAdultCategory=(callback)=>{
+//     connection.query("SELECT * FROM product WHERE category='adult'",callback)
+// }
 
-const getWomenCategory=(callback)=>{
-    connection.query("SELECT * FROM product WHERE category='women'",callback)
-}
+// const getKidsCategory=(callback)=>{
+//     connection.query("SELECT * FROM product WHERE category='kids'",callback)
+// }
 
-const getMenCategory=(callback)=>{
-    connection.query("SELECT * FROM product WHERE category='men'",callback)
-}
+// const getWomenCategory=(callback)=>{
+//     connection.query("SELECT * FROM product WHERE category='women'",callback)
+// }
 
-const getBeauty=(callback)=>{
-    connection.query("SELECT * FROM product WHERE category='beauty'",callback)
-}
+// const getMenCategory=(callback)=>{
+//     connection.query("SELECT * FROM product WHERE category='men'",callback)
+// }
 
-const addProduct=(values,callback)=>{
-    connection.query('INSERT INTO product (clothesName, image, category,price) VALUES (?, ?, ?, ?)',values,callback)
-}
+// const getBeauty=(callback)=>{
+//     connection.query("SELECT * FROM product WHERE category='beauty'",callback)
+// }
 
-
-const removeProduct=(values,callback)=>{
-    connection.query('DELETE FROM product WHERE id=?',values,callback)
-}
-
-const editProduct=(values,callback)=>{
-    const updateQuery = "UPDATE product SET clothesName = ?, image = ?, price = ?, category = ? WHERE id = ?";
-    connection.query(updateQuery,values,callback)
-}
-
-const getOneProduct=(values,callback)=>{
-    connection.query('SELECT * FROM product WHERE id=?',values,callback)
-}
-
-const searchProduct=(values,callback)=>{
-    connection.query("SELECT * FROM product WHERE clothesName LIKE ?",values,callback)
-}
+// const addProduct=(values,callback)=>{
+//     connection.query('INSERT INTO product (clothesName, image, category,price) VALUES (?, ?, ?, ?)',values,callback)
+// }
 
 
-export default {
-    getAllProducts,
-    getAdultCategory,
-    getKidsCategory,
-    getMenCategory,
-    getWomenCategory,
-    getBeauty,
-    addProduct,
-    removeProduct,
-    editProduct,
-    getOneProduct,
-    searchProduct
-}
+// const removeProduct=(values,callback)=>{
+//     connection.query('DELETE FROM product WHERE id=?',values,callback)
+// }
+
+// const editProduct=(values,callback)=>{
+//     const updateQuery = "UPDATE product SET clothesName = ?, image = ?, price = ?, category = ? WHERE id = ?";
+//     connection.query(updateQuery,values,callback)
+// }
+
+// const getOneProduct=(values,callback)=>{
+//     connection.query('SELECT * FROM product WHERE id=?',values,callback)
+// }
+
+// const searchProduct=(values,callback)=>{
+//     connection.query("SELECT * FROM product WHERE clothesName LIKE ?",values,callback)
+// }
+
+
+// export default {
+//     getAllProducts,
+//     getAdultCategory,
+//     getKidsCategory,
+//     getMenCategory,
+//     getWomenCategory,
+//     getBeauty,
+//     addProduct,
+//     removeProduct,
+//     editProduct,
+//     getOneProduct,
+//     searchProduct
+// }
