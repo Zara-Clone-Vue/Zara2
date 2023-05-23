7a4ff116683a7c61bbf70944c0b704db6d4b26e0
unknown <mouhamedarbimoussi@gmail.com>
2023-05-23 18:22:01 +0100

    done1

---
+++ frontend/src/components/ContactPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <main>
+        <button>
+ <a href="https://www.facebook.com/ZARACare">FACEBOOK</a>
+ <h5>ZARACare</h5>
+ </button>
+ <h1> </h1>
+         <button>
+ <a href="https://twitter.com/ZARA_Care">TWITTER</a>
+ <h5>@Zara_care </h5>
+ </button>
+    </main>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  // Component logic goes here
+};
+</script>
