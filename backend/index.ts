ac38084196a87ed5744b9611723bf4cb889aceda
SarhaneGhribi <sarhane.ghribi@gmail.com>
2023-05-24 14:21:43 +0100

    post working

--- backend/index.ts
+++ backend/index.ts
@@ -19,7 +19,7 @@ export const AppDataSource = new DataSource({
   host: "localhost",
   port: 3306,
   username: "root",
-  password: "root",
+  password: "Sarhane1991.",
   database: "zara",
   entities: [product],
   synchronize: true,
